508884fb7e43c673bdc1c2ea302574a4babdcaa2
Alive Company <alivecompanydg@gmail.com>
2020-03-18 16:00:28 -0300

    commits

--- src/Pages/Mindsout.js
+++ src/Pages/Mindsout.js
@@ -1,10 +1,32 @@
 
-import React from "react"
+import React, { Component } from "react"
 
-import {View, Text } from "react-native"
+import { View, Text, AsyncStorage, Button } from "react-native"
+import Speech from "expo-speech"
 
-export default function Mindsout(){
-    return(
-        <View/>
-    )
+export default class Mindsout extends Component{
+
+    state = {
+        txtSpeak: "Ola, meu nome e "
+    }
+
+     onSpeak = () => {
+        const name = AsyncStorage.getItem("asName")
+        Speech.speak(this.state.txtSpeak + name, {
+            language: "en",
+            pitch: 1,
+            rate: 1
+        })
+    }
+
+    render() {
+        return(
+            <View style={{paddingTop:100}}>
+                <Button 
+                title="Aperte"
+                onPress={this.onSpeak}
+                />
+            </View>
+        )
+    }
 }
