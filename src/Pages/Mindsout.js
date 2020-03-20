fe3b04618803cc52db88c272cda6e4f891792131
Alive Company <alivecompanydg@gmail.com>
2020-03-20 12:02:34 -0300

    commits de hoje

--- src/Pages/Mindsout.js
+++ src/Pages/Mindsout.js
@@ -1,29 +1,41 @@
 
-import React, { Component } from "react"
+import React, { useEffect, Component } from "react"
 
-import { View, Text, AsyncStorage, Button } from "react-native"
+import { View, Text, StyleSheet, AsyncStorage, Button } from "react-native"
 
 import * as Speech from "expo-speech"
 
 export default class Mindsout extends Component{
 
-    state = {
-        txtSpeak: "Ola, meu nome e "
-    }
-
-     speak() {
-        const name = AsyncStorage.getItem("asName")
-        Speech.speak(this.state.txtSpeak + name, {
-            language: "en",
-            pitch: 1,
-            rate: 1
+     async speak() {
+        const txtSpeak = "Olá, meu nome é "
+        const name = await AsyncStorage.getItem("asName").then(Name => {
+            if(Name){
+                return Name
+            }
         })
+        
+        try{
+            Speech.speak(txtSpeak + name + ", e sou seu assistente desenvolvido pela Alive Company", {
+                language: "pt",
+                pitch: 1,
+                rate: 1,
+                near: false
+            }).then(() => {
+                console.log("Yeah")
+            }).catch((err) => {
+                console.log(err)
+            })
+        }catch(err){
+            console.log(err)
+        }
     }
 
     render() {
         return(
             <View style={styles.container}>
                 <Button 
+                style={styles.button}
                 title="Aperte"
                 onPress={this.speak}
                 />
@@ -36,7 +48,11 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       justifyContent: 'center',
-      backgroundColor: '#ecf0f1',
       padding: 8
+    },
+    button: {
+        backgroundColor: '#ecf0f1',
+        width:200,
+        height:200
     }
   })
